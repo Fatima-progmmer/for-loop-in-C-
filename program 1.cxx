9f6cc9a890937d4b841aea4cbfc9c72c5bca00e6
Kashmiri Queen <149133736+Fatima-progmmer@users.noreply.github.com>
2024-02-27 09:40:36 +0500

    Add files via upload

---
+++ program 1.cxx
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x;
+	for(x=1;x<100;x++)
+	cout << "Kashmir will be indepent\n";
+	return 0;
+}
---
+++ program 2.cxx
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout <<"Enter a number";
+	cin >>x;
+	for(int x=0;x<=n,n++;)
+	cout <<"Jummu kashmir";
+	return 0;
+}
